83761ae3441425a0eb5f0a5b052dabf6cea2f2b6
Jashwanth M B <jashwanth.b@s.amity.edu>
2025-02-12 14:29:05 +0530

    multiplication of two numbers

--- add.c
+++ add.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 void main()
 { int a=10,b=12;
-	printf("addition of a and b",a+b);
-	printf("subraction of a and b",a-b);
+	printf("addition of a and b:%d",a+b);
+	printf("subraction of a and b:%d",a-b);
+	printf("multipliction of a and b:%d",a*b);
+
 }
