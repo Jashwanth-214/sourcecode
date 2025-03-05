2100cdb7f1df2eb6b7f02fee9d0e19f35a256fc7
Jashwanth M B <jashwanth.b@s.amity.edu>
2025-03-05 13:36:37 +0530

    cube of a number

---
+++ new.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{float a,val;
+	printf("enter a number");
+		scanf("%d",&a);
+    val=pow(a,3);
+    printf("cube of the num is");
+    return 0;
+}
+
