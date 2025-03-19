965396efbe3b8fb5c29e4874f25f210630928ae3
Jashwanth M B <jashwanth.b@s.amity.edu>
2025-03-19 13:56:37 +0530

    unorderd list and ordered list

---
+++ 19march/html.f
@@ -0,0 +1,22 @@
+<html>
+      <head>
+      <title>Web Technology Course</title>
+       </head>
+       <body>
+
+       <h1>Web Technology Course</h1>
+             <h2>Course Topics</h2>
+    <ul>
+        <li>HTML & CSS</li>
+        <li>JavaScript Basics</li>
+        <li>Web Hosting</li>
+    </ul>
+
+        <h2>Guest Speakers</h2>
+    <ol>
+        <li>Dr. John Doe</li>
+        <li>Prof. Jane Smith</li>
+        <li>Mr. Alex Brown</li>
+    </ol>
+      </body
+      </html>
